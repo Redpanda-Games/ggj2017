39598c9146e25d40776612306526a0a8b4d5adcb
Tom Witkowski <dev.gummibeer@gmail.com>
2017-01-21 00:32:32 +0100

    remove useless preloader code

--- www/js/elementfactory.js
+++ www/js/elementfactory.js
@@ -1,6 +1,8 @@
 ElementFactory = {
     factorShip: function() {
-
+        var sprite = this.game.add.sprite(400, 500, 'alien_ship');
+        sprite.anchor.setTo(0.5, 0.5);
+        return sprite;
     },
     factorPlanet: function() {
 
--- www/js/scenes/engine/phaser/preloader.js
+++ www/js/scenes/engine/phaser/preloader.js
@@ -5,28 +5,11 @@ var Preloader = function (game) {
 };
 Preloader.prototype = {
     preload: function () {
-        //    These are the assets we loaded in Boot.js
-        //    A nice sparkly background and a loading progress bar
         this.background = this.add.sprite(0, 0, 'preloaderBackground');
         this.preloadBar = this.add.sprite(400, 500, 'preloaderBar');
-        //this.preloadBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'preloaderBar');
-        //this.preloadBar.anchor.setTo(0.5, 0.5);
-        //    This sets the preloadBar sprite as a loader sprite.
-        //    What that does is automatically crop the sprite from 0 to full-width
-        //    as the files below are loaded in.
         this.load.setPreloadSprite(this.preloadBar);
-        //    Here we load the rest of the assets our game needs.
-        //    As this is just a Project Template I've not provided these assets, swap them for your own.
-        //this.load.image('titlepage', 'images/title.jpg');
-        //this.load.atlas('playButton', 'images/play_button.png', 'images/play_button.json');
-        //this.load.audio('titleMusic', ['audio/main_menu.mp3']);
-        //this.load.bitmapFont('caslon', 'fonts/caslon.png', 'fonts/caslon.xml');
-
-        //    + lots of other required assets here
     },
     create: function () {
-        //    Once the load has finished we disable the crop because we're going to sit in the update loop for a short while as the music decodes
-        this.preloadBar.cropEnabled = false;
         this.state.clearCurrentState();
         this.state.start('Menu');
     }
