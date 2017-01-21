4b0958610298dc0e25d5c641380c56b6a3713035
Tom Witkowski <dev.gummibeer@gmail.com>
2017-01-21 23:38:50 +0100

    add planet idle anim

--- src/js/scenes/engine/phaser/preloader.js
+++ src/js/scenes/engine/phaser/preloader.js
@@ -20,14 +20,13 @@ Preloader.prototype = {
         this.load.image('menu_background', 'src/img/menu_background.jpg');
         this.load.image('menu_start_button', 'src/img/menu_start_button.png');
         this.load.image('charackter', 'src/img/Char.png');
-        this.load.image('planet', 'src/img/planet.png');
 
         this.load.audio('attack', 'src/audio/laserattack.wav');
         this.load.audio('flysound','src/audio/space_fly.wav');
         this.load.audio('gameover','src/audio/gameover.wav');
          
+        this.load.spritesheet('planet', 'src/img/planet.png', 298, 298);
         this.load.spritesheet('ship_01', 'src/img/ship_01.png', 300, 300);
-
         this.load.spritesheet('bullet_inverse', 'src/img/bullet_inverse.png', 350, 350);
         this.load.spritesheet('bullet_increase', 'src/img/bullet_increase.png', 350, 350);
     },
