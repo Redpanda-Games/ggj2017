b0cd3ed826bd816365782fa82460c0fc860e274e
Malte Zimmermann <maziha@ewetel.net>
2017-01-21 02:33:59 +0100

    Scale

--- www/js/elementfactory.js
+++ www/js/elementfactory.js
@@ -12,7 +12,7 @@ var ElementFactory = function(game) {
             var angle = Math.atan2(_game.world.centerY - this.y, _game.world.centerX - this.x) * (180 / Math.PI);
             _game.physics.arcade.velocityFromAngle(angle, speed * this.speedMultiplier, this.body.velocity);
         };
-        sprite.scale.setTo(_game.world.width/100, _game.world.width/100);
+        sprite.scale.setTo(0.5, 0.5);
         return sprite;
     };
     this.factorPlanet = function() {
